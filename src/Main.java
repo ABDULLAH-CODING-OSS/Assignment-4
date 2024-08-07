665f6203828c1e589cc8b54f35a5d00939f140e6
ABDULLAH JAVED <jatt66016@gmail.com>
2024-08-07 21:33:09 +0500

    Task 8

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,22 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final int MOD =1000000007;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("ENTER THE STRING TO REVERSE ");
-        String s = input.nextLine();
-        int lenght=s.length();
-        System.out.println("THE REVERSED STRING IS :  "+(Reverse(s,lenght)));
+        System.out.println("ENTER THE VALUE OF P ");
+        int p = input.nextInt();
+        long result=calculateProduct(p);
+        System.out.println("THE PRODUCT IS "+(result));
 
     }
-    public static String Reverse(String s , int lenght){
-        if (lenght==0){
-            return "";
+    public static long calculateProduct(int p){
+        int limit = (1<<p)-1;
+        long product =1;
+        for (int i =1;i<=limit ; i++){
+            product=(product*i)%MOD;
         }
-        return s.charAt(lenght-1)+Reverse(s,lenght-1);
-
+        return product;
 
     }
 }
