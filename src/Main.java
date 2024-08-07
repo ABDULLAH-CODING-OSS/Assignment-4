397c81774f644354cd2e1c96f124a685f54f8e73
ABDULLAH JAVED <jatt66016@gmail.com>
2024-08-07 20:38:44 +0500

    Task 6

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("ENTER THE FIRST VALUE");
-int x = input.nextInt();
-        System.out.println("ENTER THE SECOND VALUE");
-int y= input.nextInt();
-int res = GCD(x,y);
-        System.out.println("THE GCD OF THESE NUMBERS ARE "+(res));
-
+        System.out.println("ENTER THE BASE ");
+        int base = input.nextInt();
+        System.out.println("ENTER THE EXPONENT ");
+        int exponent=input.nextInt();
+        int res = Power(base, exponent);
+        System.out.println("THE ANSWER IS "+(res));
 
 
 
     }
-    public static int GCD(int x , int y){
-     if (y==0){
-         return x;
-     }
-      return GCD(y,x%y);
+    public static int Power(int b , int e){
+        if (e==1){
+            return b;
+        }
+        return b * Power(b,e-1);
+
 
     }
 }
