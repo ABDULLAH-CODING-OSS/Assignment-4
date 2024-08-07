c0617eb332f2260318aa4bd0880461e7994f7769
ABDULLAH JAVED <jatt66016@gmail.com>
2024-08-07 19:30:10 +0500

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
    Scanner input = new Scanner(System.in);
-        System.out.println("ENTER YOUR NUMBER  ");
-         int number = input.nextInt();
-        System.out.println("ENTER THE NUMBER TO BE SEARCHED");
-         int searchNumber=input.nextInt();
-        int result = Count(number,searchNumber);
-        System.out.println("The number "+(searchNumber)+" appears "+(result)+" Times");
+        System.out.println("ENTER THE NUMBER OF Men ");
+        int hs=input.nextInt();
+        int res = handSShake(hs);
+        System.out.println("TOTAL HANDSHAKES BY "+(hs)+" Mens are "+(res));
+
 
      }
-     public static int Count(int number ,int digit){
-        if (number==0){
-           return  (digit==0)?1:0;
+     public static int handSShake(int n){
+        if (n<=1){
+            return 0;
         }
-        int lastDigit = number%10;
-        int count = (lastDigit==digit)?1:0;
-        return count + Count(number/10,digit);
-
-
-
-
+        return handSShake(n-1)+(n-1);
      }
 
 }
